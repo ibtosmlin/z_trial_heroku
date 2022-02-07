f2c9adf89260ec252d4e503f338f04a36d411cf5
ibtosmlin <63023191+ibtosmlin@users.noreply.github.com>
2022-02-07 14:49:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,36 @@ import os
 import readcsv
 import datetime
 
+
 app = Flask(__name__)
 
 # 更新日時
 update_time =  readcsv.get_update_time()
 header, contents = readcsv.get_table()
-d_today = datetime.date.today()
+d_today = str(datetime.date.today())
+date_fm = "2000-04-01"
+date_to = d_today
 
-print(str(d_today))
 @app.route("/", methods=['GET', 'POST'])
 def script():
-
+    global date_fm, date_to
     if request.method == 'GET':
         return render_template('./index.html',
-                update_time=update_time, header=header)
+                update_time=update_time, header=header,
+                datefrom = date_fm, dateto = date_to
+                )
+
     elif request.method == 'POST':
-        datefrom = request.form['datefrom']
-        dateto = request.form['dateto']
+        date_fm = request.form['datefrom']
+        date_to = request.form['dateto']
         company = request.form['company']
         kwrds = request.form['company']
-        tabledata = readcsv.select_table(header, contents, datefrom, dateto, company, kwrds)
+        tabledata = readcsv.select_table(header, contents, date_fm, date_to, company, kwrds)
         return render_template('./index.html',
                 update_time=update_time, header=header,
-                d_today = str(d_today),
-                tabledata=tabledata)
+                datefrom = date_fm, dateto = date_to,
+                tabledata=tabledata
+                )
 
 #def links(url)
 
@@ -34,3 +40,4 @@ def script():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+

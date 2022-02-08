1e30eb78ee9f633eb617726e7902dd14fcfcded9
atabiwebs <atabi.036@gmail.com>
2022-02-08 19:57:28 +0900

    update

--- data/readcsv.py
+++ data/readcsv.py
@@ -7,20 +7,27 @@ libpath = os.path.dirname(__file__)
 filename = os.path.join(libpath, 'test.csv')
 header_def = os.path.join(libpath, 'test_header.csv')
 update_date = os.path.join(libpath, 'update_date.csv')
+comp_url = os.path.join(libpath, 'list_li_utf8.csv')
+
+
+with open(comp_url, encoding='utf-8-sig', newline='') as f:
+    csvreader = csv.reader(f, delimiter=",", doublequote=True)
+    comp_url_dict = {row[0]: row[1] for row in csvreader}
+
 
 def get_table():
-    with open(header_def, encoding='utf8', newline='') as f:
+    with open(header_def, encoding='utf-8-sig', newline='') as f:
         csvreader = csv.reader(f, delimiter=",", doublequote=True)
         header = [row for row in csvreader]
         header = header[0]
-    with open(filename, encoding='utf8', newline='') as f:
+    with open(filename, encoding='utf-8-sig', newline='') as f:
         csvreader = csv.reader(f, delimiter=",", doublequote=True)
-        content = [row for row in csvreader]
+        content = [row + [comp_url_dict[row[0]]] for row in csvreader]
     return header, content
 
 
 def get_update_time():
-    with open(update_date, encoding='utf8', newline='') as f:
+    with open(update_date, encoding='utf-8-sig', newline='') as f:
         csvreader = csv.reader(f, delimiter=",", doublequote=True)
         content = [row for row in csvreader]
     return content[0][0]
@@ -29,8 +36,8 @@ def get_update_time():
 def select_table(head, contents, fm, to, comp, kwd):
     ret = []
     for con in contents:
-        if comp == con[0]:
-            ret.append(con)
+#        if comp == con[0]:
+        ret.append(con)
     return ret
 
 

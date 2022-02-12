b26ba4b1b4c471dba4c17ade098ee246421a88cd
ibtosmlin <ibt.osm@gmail.com>
2022-02-12 18:27:38 +0900

    following box

---
+++ static/js/followingbutton.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#topBtn').click(function () { // #topBtnをクリックすると
+        $('body,html').animate({ // いちばん上にanimateする
+        scrollTop: 0 // 戻る位置
+        }, 400); // 戻るスピード
+        return false;
+    });
+});

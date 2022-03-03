8c165c33c988f440fa65f18e3648b39940a9f0d7
ibtosmlin <63023191+ibtosmlin@users.noreply.github.com>
2022-03-04 07:08:21 +0900

    Update searchmenu.js

--- static/js/searchmenu.js
+++ static/js/searchmenu.js
@@ -6,11 +6,15 @@ $(function () {
 
   if(sIcon.text() == "search") {
     sIcon.text("close");
+    // 表示
+    Menu.firstChild.removeClass("menu_child_shrink");
     Menu.removeClass("menu_shrink");
     Menu.addClass("menu_dropdown");
   }
   else {
     sIcon.text("search");
+    // 非表示
+    Menu.firstChild.addClass("menu_child_shrink");
     Menu.removeClass("menu_dropdown");
     Menu.addClass("menu_shrink");
   }

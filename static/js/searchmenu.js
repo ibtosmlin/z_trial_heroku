a59da5e53af61bb8becd9a765f770c669b9b1cce
ibtosmlin <ibt.osm@gmail.com>
2022-03-06 18:09:40 +0900

    media

--- static/js/searchmenu.js
+++ static/js/searchmenu.js
@@ -1,47 +1,23 @@
 $(function () {
-  var sIcon = $('#searchicon');
-  var Menu = $('#menu');
-  var Menu_c = $('#menu_child');
-  //ボタン表示
-  sIcon.click(function (){
+  var $sibox = $('#sicheckbox');
+  var $Menu = $('#Menu');
+  var $Menu_c = $('#Menu_child');
+  var $rbox = $("#resultbox");
 
-  if(sIcon.text() == "search") {
-    //icon
-    cIcon.removeClass
+  $sibox.change(function(){
+  if($sibox.prop("checked") == true) {
     // 表示
-    Menu_c.removeClass("menu_child_shrink");
-    Menu.removeClass("menu_shrink");
-    Menu.addClass("menu_dropdown");
+    $rbox.addClass("rbox_shrink");
+    $Menu_c.removeClass("Menu_child_shrink");
+    $Menu.removeClass("Menu_shrink");
+    $Menu.addClass("Menu_dropdown");
+
   }
   else {
-//    sIcon.text("search");
     // 非表示
-    Menu_c.addClass("menu_child_shrink");
-    Menu.removeClass("menu_dropdown");
-    Menu.addClass("menu_shrink");
-  }
-})
+    $Menu_c.addClass("Menu_child_shrink");
+    $Menu.removeClass("Menu_dropdown");
+    $Menu.addClass("Menu_shrink");
+    $rbox.removeClass("rbox_shrink");
+  }})
 });
-
-
-$(function(){
-
-  var rbox = $("#resultbox")
-  var card = $(".card")
-
-  let emptyCells = [];//空の配列を作っておく
-  let parentWidth = rbox.width();//rbox(sectionの親要素)の幅を取得
-  let childWidth = card.outerWidth(true);//list-itemnの幅を取得
-  let column = parentWidth/childWidth;//親要素の幅を子要素の幅を割ったものがカラム数になる
-  column = Math.floor(column) ;//カラム数の小数点切り捨て
-  let count = card.length;//list-itemの数を取得
-  let number = count%column;//list-itemの数をカラムで割った余りを出す、余りの値が最後列の要素数になる。
-  let need = column - number;//カラム数から最後列の要素数を引いたものが、必要な要素数になる。
-
-  for (i = 0; i < need; i++) {
-     emptyCells.push($('<section>', { class: 'blankcard' }));//必要分の空のを配列に追加
-  }
-
-  rbox.append(emptyCells);//list-itemの最後に空の要素を追加する。
-
- });

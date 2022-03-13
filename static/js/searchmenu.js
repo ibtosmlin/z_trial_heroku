54fedd8072af98e31aa20fc75b050a72424c1398
ibtosmlin <ibt.osm@gmail.com>
2022-03-13 20:52:33 +0900

    animation

--- static/js/searchmenu.js
+++ static/js/searchmenu.js
@@ -7,13 +7,15 @@ $(function () {
   $Si.click(function(){
     if($Si.text() == 'search') {
     // si: close Menu: 表示; rbox: 非表示
+      $Si.toggleClass('siactive');//ナビゲーションにpanelactiveクラスを付与
       $Si.text('close')
       $Menu.toggleClass('siactive');//ナビゲーションにpanelactiveクラスを付与
       $Circle.toggleClass('siactive');//丸背景にcircleactiveクラスを付与
     }
     else {
     // si: serch Menu: 非表示; rbox: 表示
-      $Si.text('search')
+    $Si.removeClass('siactive');//ナビゲーションのpanelactiveクラスを除去
+    $Si.text('search')
       $Menu.removeClass('siactive');//ナビゲーションのpanelactiveクラスを除去
       $Circle.removeClass('siactive');//丸背景のcircleactiveクラスを除去
     }

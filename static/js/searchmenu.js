7ea6d74cf677f92f978dd5ccd634ee4a1cbd0737
ibtosmlin <ibt.osm@gmail.com>
2022-03-14 21:12:11 +0900

    animation

--- static/js/searchmenu.js
+++ static/js/searchmenu.js
@@ -1,23 +1,40 @@
 $(function () {
+  var $Sistatus = true;
+  var $Sisearch = $('#search');
+  var $Siclose = $('#close');
   var $Si = $('#searchicon');
   var $Menu = $('#menu');
   var $Circle = $("#circle-bg");
-//  var $Menu_c = $('#menu_child');
+
 
   $Si.click(function(){
-    if($Si.text() == 'search') {
-    // si: close Menu: 表示; rbox: 非表示
-      $Si.toggleClass('siactive');//ナビゲーションにpanelactiveクラスを付与
-      $Si.text('close')
-      $Menu.toggleClass('siactive');//ナビゲーションにpanelactiveクラスを付与
-      $Circle.toggleClass('siactive');//丸背景にcircleactiveクラスを付与
-    }
+    if($Sistatus) {
+      // si: close Menu: 表示; rbox: 非表示
+      $Sisearch.css('display','none');
+      $Sisearch.css('opacity',0);
+      $Siclose.css('display','block');
+      $Siclose.animate({opacity: 1}, 1000);
+
+      $Circle.css('z-index', 80);
+      $Circle.css( { transform: "scale(50)" } );
+      $Menu.css('z-index', 100);
+      $Menu.animate({opacity:1}, 1000);
+
+      $Sistatus = false;
+  }
     else {
-    // si: serch Menu: 非表示; rbox: 表示
-    $Si.removeClass('siactive');//ナビゲーションのpanelactiveクラスを除去
-    $Si.text('search')
-      $Menu.removeClass('siactive');//ナビゲーションのpanelactiveクラスを除去
-      $Circle.removeClass('siactive');//丸背景のcircleactiveクラスを除去
+      // si: serch Menu: 非表示; rbox: 表示
+      $Siclose.css('display','none');
+      $Siclose.css('opacity',0);
+      $Sisearch.css('display','block');
+      $Sisearch.animate({opacity: 1}, 1000);
+
+      $Menu.animate({opacity:0}, 800);
+      $Menu.css('z-index', 10);
+      $Circle.css( { transform: "scale(0)" } );
+      $Circle.css('z-index', 10);
+
+      $Sistatus = true;
     }
   })
 });
